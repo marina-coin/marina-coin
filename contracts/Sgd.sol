f854401ffbd307cb993d9176415c18054d495be4
LeoKnight <lichenghua0@outlook.com>
2021-05-05 02:56:42 +0800

    1

---
+++ contracts/Sgd.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.5.16;
+
+contract Sgd {
+    function helloWorld() external pure returns (string memory) {
+        return "Hello, World!";
+    }
+}
